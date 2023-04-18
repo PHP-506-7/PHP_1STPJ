e4a2f0f9649b997a5207d8b08d5f556bc696ea46
SuyeonParka <sysysy3331@gmail.com>
2023-04-18 12:16:07 +0900

    0418 1st pj sql up

---
+++ doc/sql/create_recom_routine.sql
@@ -0,0 +1,12 @@
+CREATE DATABASE todo;
+
+USE todo;
+
+CREATE TABLE recom_routine(
+	recom_no INT PRIMARY KEY AUTO_INCREMENT  
+	,recom_title VARCHAR(33) NOT NULL 
+	,recom_contents VARCHAR(50) NOT NULL 
+	);
+	
+
+	
