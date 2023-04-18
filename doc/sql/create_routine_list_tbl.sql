3742c60d0c840f60ba7e06824944bd51ec2668a9
rlawotjd <rlawotjd026@gmail.com>
2023-04-18 12:17:57 +0900

    0418 sql create

---
+++ doc/sql/create_routine_list_tbl.sql
@@ -0,0 +1,10 @@
+CREATE TABLE routine_list(
+	list_no INT PRIMARY KEY AUTO_INCREMENT
+	,routine_no INT NOT NULL 
+	,list_title VARCHAR(33) NOT NULL
+	,list_contents VARCHAR(50) NOT NULL
+	,list_due_time DATETIME
+	,list_done_flg CHAR(1) DEFAULT '0'
+	,list_now_date DATETIME DEFAULT NOW()
+	)
+;
