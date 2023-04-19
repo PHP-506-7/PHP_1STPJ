1d0afe531eb067aacd9e9b53fbff376b0c99e481
SuyeonParka <sysysy3331@gmail.com>
2023-04-19 16:33:16 +0900

    0419 sql up

--- doc/sql/create_routine_list_tbl.sql
+++ doc/sql/create_routine_list_tbl.sql
@@ -3,9 +3,9 @@ CREATE TABLE routine_list(
 	,routine_no INT NOT NULL 
 	,list_title VARCHAR(33) NOT NULL
 	,list_contents VARCHAR(50) NOT NULL
-	,list_due_time DATETIME
+	,list_due_time TIME NOT NULL 
 	,list_done_flg CHAR(1) DEFAULT '0'
 	,list_now_date DATETIME DEFAULT NOW()
 	)
 ;
-commit;
+COMMIT;
