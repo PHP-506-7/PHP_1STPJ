066a472c5a71407df7ae7f74814cb4a14a096ca5
choooii <dkfks0832@gmail.com>
2023-04-19 16:34:44 +0900

    0419 sql routine _info udt

--- doc/sql/create_routine_info_tbl.sql
+++ doc/sql/create_routine_info_tbl.sql
@@ -4,10 +4,8 @@ CREATE TABLE routine_info (
 	routine_no INT PRIMARY KEY AUTO_INCREMENT
 	,routine_title VARCHAR(33) NOT NULL
 	,routine_contents VARCHAR(50) NOT NULL
-	,routine_due_date DATETIME NOT NULL
+	,routine_due_time TIME NOT NULL
 	,routine_write_date DATETIME DEFAULT NOW()
 	,routine_del_flg CHAR(1) DEFAULT '0'
 	,routine_del_date DATETIME NULL
 );
-
-COMMIT;
